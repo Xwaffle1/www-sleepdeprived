d6d7ad7e131ecbf03db9d00d60ff060e852ea45a
Xwaffle1 <chasecool56@gmail.com>
2024-02-07 15:37:41 -0500

    banger mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,19 +20,19 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <div className="flex w-full h-32 fixed z-40">
-        <div className="w-1/3 h-1/2 bg-black rounded-b-md rounded-r-none z-40 text-white flex items-center px-4 gap-2">
+        <div className="w-1/3 h-1/2 bg-black rounded-b-md rounded-r-none z-40 text-white flex items-center px-4 pr-0 md:pr-4 gap-2">
           <MapPinIcon  className="w-6 h-6"/>
-          <p className="text-xs md:text-lg">Jackson, MI</p>
+          <p className="text-xs md:text-lg font-semibold">Jackson, MI</p>
         </div>
-        <div className="flex w-1/3 justify-center bg-black rounded-b-md z-40">
-          <div className="flex justify-center relative w-full">
-            <Image className="w-auto" src="/logo.png" alt="logo" width={100} height={100} />
+        <div className="flex w-1/3 justify-center bg-black rounded-b-md z-40 overflow-hidden">
+          <div className="flex justify-center relative w-full bg-black">
+            <img className="w-auto bg-black rounded-b-md" src="/logo.png" alt="logo" />
           </div>
         </div>
         <div className="w-1/3 h-1/2 bg-black rounded-b-md rounded-l-none flex items-center justify-end px-4 z-100">
-          <button className="bg-red-600 hover:bg-red-700 rounded-md p-2 text-white hover:text-gray-300 font-bold">
+          <a href="https://www.etsy.com/shop/IAmSleepDeprived?ref=sleepdeprived.app" className="bg-red-600 hover:bg-red-700 rounded-md p-2 text-white hover:text-gray-300 font-bold">
             Shop Now
-          </button>
+          </a>
         </div>
       </div>
       <div className="w-full h-8">
--- app/page.tsx
+++ app/page.tsx
@@ -48,8 +48,8 @@ export default function Home() {
         <h2 className="text-2xl font-bold text-center my-8">
           Featured Products
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-8 p-4">
-          <a href="" className="flex flex-col items-center text-center group">
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-8 p-4">
+          <a href="https://www.etsy.com/listing/1635913296/sleep-deprived-embroidered-t-shirt" className="flex flex-col items-center text-center group">
             <img
               alt="Product 1"
               className="w-full h-auto object-cover rounded-t-xl max-h-72 p-2 bg-white"
@@ -57,10 +57,10 @@ export default function Home() {
             />
             <div className="bg-gray-600 group-hover:bg-red-600 w-full rounded-b-xl p-2">
               <h3 className="text-white  text-lg font-semibold">embroidered t-shirt</h3>
-              <p className="text-gray-300 ">$25.00</p>
+              {/* <p className="text-gray-300 ">$25.00</p> */}
             </div>
           </a>
-          <a href="" className="flex flex-col items-center text-center group">
+          <a href="https://www.etsy.com/listing/1650081177/sleep-deprived-embroidered-beanie" className="flex flex-col items-center text-center group">
             <img
               alt="Product 1"
               className="w-full h-auto max-h-72 object-cover rounded-t-xl p-2 bg-white"
@@ -68,10 +68,10 @@ export default function Home() {
             />
             <div className="bg-gray-600 group-hover:bg-red-600 w-full rounded-b-xl p-2">
               <h3 className="text-white text-lg font-semibold">embroidered beanie</h3>
-              <p className="text-gray-300">$20.00</p>
+              {/* <p className="text-gray-300">$20.00</p> */}
             </div>
           </a>
-          <a href="" className="group flex flex-col items-center text-center">
+          <a href="https://www.etsy.com/listing/1650077429/sleep-deprived-embroidered-crewneck" className="group flex flex-col items-center text-center">
             <img
               alt="Product 1"
               className="w-full h-auto object-cover rounded-t-xl max-h-72 p-2 bg-white"
@@ -79,10 +79,10 @@ export default function Home() {
             />
             <div className="bg-gray-600 group-hover:bg-red-600 w-full rounded-b-xl p-2">
               <h3 className="text-white text-lg font-semibold">embroidered sweater</h3>
-              <p className="text-gray-300">$20.00</p>
+              {/* <p className="text-gray-300">$20.00</p> */}
             </div>
           </a>
-          <a href="" className="group flex flex-col items-center text-center h-full">
+          <a href="https://www.etsy.com/listing/1637420632/sleep-deprived-embroidered-hoodie" className="group flex flex-col items-center text-center h-full">
             <img
               alt="Product 1"
               className="w-full h-auto max-h-72 object-scale-down rounded-t-xl p-2 bg-white"
@@ -90,29 +90,29 @@ export default function Home() {
             />
             <div className="bg-gray-600 group-hover:bg-red-600 w-full rounded-b-xl p-2">
               <h3 className="text-white text-lg font-semibold">embroidered hoodie</h3>
-              <p className="text-gray-300">$20.00</p>
+              {/* <p className="text-gray-300">$20.00</p> */}
             </div>
           </a>
-          <a href="" className="group flex flex-col items-center text-center h-full">
+          <a href="https://www.etsy.com/listing/1637418642/sleep-deprived-embroidered-baseball-cap" className="group flex flex-col items-center text-center h-full">
             <img
-              alt="Product 1"
+              alt="Product hat"
               className="w-full h-auto object-cover rounded-t-xl max-h-72 p-2 bg-white"
               src="/hat stock.jpg"
             />
             <div className="bg-gray-600 group-hover:bg-red-600 w-full rounded-b-xl p-2">
-              <h3 className="text-white text-lg font-semibold">embroidered hat</h3>
-              <p className="text-gray-300">$20.00</p>
+              <h3 className="text-white text-lg font-semibold">embroidered baseball hat</h3>
+              {/* <p className="text-gray-300">$20.00</p> */}
             </div>
           </a>
-          <a href="" className="flex flex-col items-center text-center h-full group">
+          <a href="https://www.etsy.com/listing/1651588621/sleep-deprived-embroidered-bucket-hat" className="flex flex-col items-center text-center h-full group">
             <img
-              alt="Product 1"
+              alt="Product bucket hat"
               className="w-full object-fit rounded-t-xl h-72 p-2 bg-white"
               src="/bucket stock.jpg"
             />
             <div className="bg-gray-600 group-hover:bg-red-600 w-full rounded-b-xl p-2">
               <h3 className="text-white text-lg font-semibold">embroidered bucket hat</h3>
-              <p className="text-gray-300">$20.00</p>
+              {/* <p className="text-gray-300">$20.00</p> */}
             </div>
           </a>
         </div>
